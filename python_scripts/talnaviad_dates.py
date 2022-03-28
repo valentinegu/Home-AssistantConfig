09bee1cdc31d48566809431ad9e9cc00fac7e5b2
valentinegu <admin@networklabs.info>
2022-03-28 20:32:50 +0100

    pereodic

--- python_scripts/talnaviad_dates.py
+++
@@ -1,9 +0,0 @@
-res = []
-WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-for i in range(13):
-    d = datetime.datetime.now()-datetime.timedelta(days=i)
-    if not d.weekday() in [4,5]:
-        res.append(f'{WEEKDAYS[d.weekday()]} {d.day}/{d.month:02}/{d.year-2000}')
-service_data = {'entity_id': 'input_select.tal_aviad', 'options': res}
-hass.services.call('input_select', 'set_options', service_data)
-
